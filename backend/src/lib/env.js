afb2a235aea17a7cced1540c20662f202a4672c0
Jatin Singh Patwal <jatinpatwal011@gmail.com>
2026-02-05 10:53:21 +0530

    upated-profile added

---
+++ backend/src/lib/env.js
@@ -0,0 +1,11 @@
+import "dotenv/config"
+
+export const ENV = {
+    PORT: process.env.PORT,
+    MONGO_URL: process.env.MONGO_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    NODE_ENV: process.env.NODE_ENV,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import router from "./routes/auth.route.js";
 import path from "path";
@@ -8,6 +9,7 @@ const app = express();
 const _dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth", router);
 // app.use("/api/messages", message);
 
